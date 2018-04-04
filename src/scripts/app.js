c156d57c2d061cf8c74e5641c5a2fca6670cf08c
Vadim Tumanov <tumanoff.vadim@yandex.ru>
2018-04-04 19:27:25 +0300

    Map

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
 const slider = require('./common/slider');
+const map = require('./common/map');
 
+initMap();
 slider(); // инициализируем слайдер
--- src/scripts/common/map.js
+++ src/scripts/common/map.js
@@ -1,26 +1,14 @@
-var GoogleMapsloader = require("google-map");
+function initMap(){
+  var element = document.getElementById('map');
+  var options = {
+    zoom: 10,
+    center: {lat: 54.609543, lng: 39.712585}
+  };
 
-module.exports = mapSelector => {
-  let el = document.querySelector ("#" + mapSelector);
-  if (el){
-    var ufa ={
-      lat: 54.814602,
-      lng: 56.886682
-    };
-    var zoomVal = 15;
+  var myMap = new google.maps.Map(element, options);
 
-    var screenWidth = document.body.clientWidth;
-    if (screenWidth <= 1024) zoomval = 14;
-    if (screenWidth <= 480) zoomval = 13;
-
-
-    GoogleMapsloader.load(function(google){
-      var map = new google.maps.Map(el,{
-        zoom: zoomVal,
-        center: ufa,
-        mapTypeControl: false,
-        disableDefaultUI: true,
-      })
-    })
-  }
-};
+  var marker = new google.maps.Marker({
+    position: {lat: 54.63463, lng: 39.692274},
+    map: myMap
+  });
+}
