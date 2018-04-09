ca4876bac4e720ffa9e3f9b875708e376b1dc6f1
Vadim Tumanov <tumanoff.vadim@yandex.ru>
2018-04-09 03:08:13 +0300

    Welcome page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,17 @@
-const slider = require('./common/slider');
-const map = require('./common/map');
+// // const slider = require('./common/slider');
+// // const map = require('./common/map');
 
-initMap();
-slider(); // инициализируем слайдер
+// // initMap();
+// // slider(); // инициализируем слайдер
+
+// const parallaxContainer = document.getElementById('parallax');
+// const layers = parallaxContainer.children;
+
+// const moveLaters = e => {
+//   const initialX = window.innerWidth / 2 - e.pageX;
+//   const initialY = window.innerWidth / 2 - e.pageY;
+//   console.log(initialX, initialY);
+  
+// };
+
+// window.addEventListener('mousemove', moveLayers);
--- src/scripts/common/map.js
+++ src/scripts/common/map.js
@@ -1,14 +1,14 @@
-function initMap(){
-  var element = document.getElementById('map');
-  var options = {
-    zoom: 10,
-    center: {lat: 54.609543, lng: 39.712585}
-  };
+// function initMap(){
+//   var element = document.getElementById('map');
+//   var options = {
+//     zoom: 10,
+//     center: {lat: 54.609543, lng: 39.712585}
+//   };
 
-  var myMap = new google.maps.Map(element, options);
+//   var myMap = new google.maps.Map(element, options);
 
-  var marker = new google.maps.Marker({
-    position: {lat: 54.63463, lng: 39.692274},
-    map: myMap
-  });
-}
+//   var marker = new google.maps.Marker({
+//     position: {lat: 54.63463, lng: 39.692274},
+//     map: myMap
+//   });
+// }
